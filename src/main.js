906e7bf8d1a7870c305782f7824953a4a88d97ab
Sudharshan <sudhar393@gmail.com>
2018-08-05 09:17:56 +0800

    partially working AJAX

--- src/main.js
+++ src/main.js
@@ -12,7 +12,17 @@ httpRequest.send();
 function updatePosts() {
     if(httpRequest.readyState === XMLHttpRequest.DONE){
         if(httpRequest.status === 200) {
-            console.log(httpRequest.responseXML);
+            console.log(httpRequest.responseText);
+            res = JSON.parse(httpRequest.responseText);
+            document.querySelector('#blog-post-header-1').innerText = res.posts[0].title;
+            document.querySelector('#blog-post-header-2').innerText = res.posts[1].title;
+            
+            var post1_text = res.posts[0].plaintext;
+            var post2_text = res.posts[1].plaintext;
+
+            document.querySelector('#blog-post-text-1').innerText = post1_text.substr(0, 100);
+            document.querySelector('#blog-post-text-2').innerText = post2_text.substr(0, 100);
+
         } else {
             console.log("An error has occurred with the response")
         }
